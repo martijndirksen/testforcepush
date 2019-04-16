76dee71982e8b3af6a4698f11da5a2362cdfbaa8
Martijn Dirksen <mo.dirksen@gmail.com>
2019-04-16 18:55:25 +0200

    B

--- TestForcePush/Program.cs
+++ TestForcePush/Program.cs
@@ -6,7 +6,7 @@ namespace TestForcePush
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World! Ver 1.0");
+      Console.WriteLine("Hello World! Ver 2.0");
     }
   }
 }
